898d5c96e882c46be1b1f73616fdf46bee2dddbd
Max Henger <MaxHenger@Maxs-MacBook-Pro.local>
2016-05-03 12:33:14 +0200

    initial commit

---
+++ Atmosphere.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 12:29:47 2016
+
+@author: MaxHenger
+"""
+
+# __determineInterpolationConstants is a helper functions to pre-calculate the
+# interpolation coefficients required for interpolation between the atmospheric
+# properties of Venus. This way the interpolation algorithm deriving these 
+# coefficients does not have to be run each time a certain value is required.
+def __determineInterpolationConstants__():
+    
+
+class Atmosphere:
+    def __init__(self):
+        self.values = 0
+        
+    
