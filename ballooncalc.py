5b7d8ecca99cdfe0769feabd44c8703e1707adeb
juriasu <weinmiller@live.co.uk>
2016-05-09 14:20:36 +0200

    fixes

--- ballooncalc.py
+++ ballooncalc.py
@@ -111,7 +111,9 @@ def Solarpanelpower(Vbal, Thickcord, Aspect, alt):
 
 if __name__=="__main__":
     
+    mpayload = 4500.
     hbuoyancy = 50000
+    structurPayloadFactor=0.2
     m_molar = 2.016
     
     expanFactor = 0.1
