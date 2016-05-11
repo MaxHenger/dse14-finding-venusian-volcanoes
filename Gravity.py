c274c6d2382a1c4e46485a1d24b199c78878235f
juriasu <weinmiller@live.co.uk>
2016-05-11 17:09:03 +0200

    Megallen option
    Added the megallen option for gravity calculations
    Much MUCH MUCH more accurate, but also slower

--- Gravity.py
+++ Gravity.py
@@ -12,7 +12,7 @@ arrays as much as possible to reduce unneccesary calculations.
 The general use of this file is as following:
     
     import Gravity
-    grav = Gravity.Gravity(Venus)
+    grav = Gravity.Gravity()
     grav(alt,latitude,longitude)
     
 @author: Julius
@@ -26,13 +26,26 @@ import GravityConstants as grav_const
 
 class Gravity:
     """Gravity class to get the gravity at a specific point"""
-    def __init__(self,planet="Venus"):
+    def __init__(self,planet="Venus",GravModel="Magellen",accuracy=10):
         if planet=="Venus":
-            print 
-            self.constants=grav_const.GravityVenus()
+            self.constants=grav_const.GravityVenus(GravModel,accuracy)
+            self.GravModel=GravModel
+            self.accuracy=accuracy
+            if GravModel=="Magellen":
+                self.S=self.constants.S
+                self.C=self.constants.C
+            elif GravModel=="simple":
+                self.J=self.constants.J
+                self.lam=self.constants.lam
+            self.R=self.constants.RadiusMean
+        
+        elif planet=="Earth":
+            self.constants=grav_const.GravityEarth()
             self.J=self.constants.J
             self.lam=self.constants.lam
+            self.GravModel="simple"
             self.R=self.constants.RadiusMean
+            
         else:
             raise ValueError("Planet Unknown")
         
@@ -48,23 +61,36 @@ class Gravity:
         
     def __P2__(self,n,m,x):
         z=sympy.Symbol("z")
-        y = self.__P1__(n,z)
+        y = 1./ ( (-2)**n * util.factorial(n) ) * (1-z**2)**n
         yprime = y.diff(z,m)
         func = sympy.lambdify(z,yprime,'numpy')
         value = (1.-x**2)**(m/2.) * func(x) # * diff m self.__P1__(n,x)
         return value
     
     def __gravity__(self,altitude,longitude,latitude):
-        r = altitude+self.R
-        g = self.constants.Mu/(r)**2 * ( 1 \
--sum([ self.J[n] * (self.R/r)**n * self.__P1__(n,np.sin(np.deg2rad(latitude))) for n in range(2,len(self.J))]) \
-+sum([ sum([ self.J[n][m]*(self.R/r)**n*self.__P2__(n,m,np.sin(np.deg2rad(latitude)))*np.cos(m*(longitude-self.lam[n,m])) \
-    for m in range(1,n)]) for n in range(2,len(self.J))  ]) )
-        return g
+        r0 = altitude+self.R
+        r = sympy.Symbol("r")
+        if self.GravModel=="simple":
+            U = -self.constants.Mu/(r) * ( 1 \
+    -sum([ self.J[n][0] * (self.R/r)**n * self.__P1__(n,np.sin(np.deg2rad(latitude))) for n in range(2,len(self.J))]) \
+    +sum([ sum([ self.J[n][m]*(self.R/r)**n*self.__P2__(n,m,np.sin(np.deg2rad(latitude)))*np.cos(m*(np.deg2rad(longitude-self.lam[n,m]))) \
+        for m in range(1,n)]) for n in range(2,len(self.J))  ]) )
+            
+        elif self.GravModel=="Magellen":
+            U = -self.constants.Mu/(r) * ( 1 \
+    -sum([ -self.C[n][0] * (self.R/r)**n * self.__P1__(n,np.sin(np.deg2rad(latitude))) for n in range(2,len(self.C))]) \
+    +sum([ sum([ (self.R/r)**n*self.__P2__(n,m,np.sin(np.deg2rad(latitude)))*(self.C[n][m]*np.cos(m*np.deg2rad(longitude)) + self.S[n][m]*np.sin(m*np.deg2rad(longitude)) ) \
+        for m in range(1,n)]) for n in range(2,len(self.C))  ]) )
+        
+        else:
+            raise ValueError("unknown GravModel")
+            
+        Uprime = U[1].diff(r)
+        func = sympy.lambdify(r,Uprime,'numpy')
+        return func(r0)
         
     def __testJ__(self):
         r = self.R
         lat=np.arange(0,90,15)
         for latitude in lat:
             print(str(latitude)+": ", [ self.J[n] * (self.R/r)**n * self.__P1__(n,np.sin(np.deg2rad(latitude))) for n in range(2,len(self.J))]   )
-        
