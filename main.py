e13f3d285b7a5f1fb2876502cf66e38406bab667
Stefanharmsen <stefanhar@hetnet.nl>
2016-05-19 14:47:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def structuremass(rhofuselage,rhowing,Efuselage,Ewing,V,rhoair,Volume, L, Loc1, 
         Vzf=fuselageloadcase[1]
         Mzf=fuselageloadcase[2]
         Myf=fuselageloadcase[3]
-        fuselagestresses=fuselagestress(R,sufuselage,Vyf,Vzf,Mzf,Myf,tf,SF,p)
+        fuselagestresses=fuselagestress(R,sufuselage,Vyf,Vzf,Mzf,Myf,tf,SF,p,L)
         wingstresses=wingstress(I,Ewin,Vy,Vz,Mz,My,structure,tw,c,SF)
         tf2=fuselagestresses
         tw2=wingstresses[0]
@@ -78,4 +78,42 @@ def structuremass(rhofuselage,rhowing,Efuselage,Ewing,V,rhoair,Volume, L, Loc1, 
     Mf=fuselagemass(t,L,R,rhofuselage)
     Mw=wingmass(,,rhowing)#multiply with constant for stuff other than wingbox
     Mstructure=Mf+Mw
+    fuselage=fuselagestress(R,sufuselage,Vyf,Vzf,Mzf,Myf,tf,SF,p,L)
+    x=np.array(fuselage[1])
+    y=np.array(fuselage[2])
+    z=np.array(fuselage[3])
+    vMs=np.array(fuselage[4])
+    #dz=fuselage[5]
+    
+    
+    #map(Y.extend,y)
+    #map(Z.extend,z)
+    import matplotlib
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111, projection='3d')
+    
+    #ax.plot_surface(Z,X,Y,rstride=1, cstride=1)#color=vMs
+    #plt.legend()
+    #plt.show()
+    from mayavi import mlab
+    from itertools import chain
+    X=list(chain.from_iterable(x))
+    Y=list(chain.from_iterable(y))
+    Z=list(chain.from_iterable(z))
+    #print np.shape(Y)
+    #print np.shape(Z)
+    #print np.shape(vMs)
+    def plot_mayavi(structured_data):
+        sigma = structured_data[3]
+        fig = mlab.figure(bgcolor=(1,1,1),fgcolor=(0,0,0))
+        #obj=mlab.contour3d(x, y, z,sigma)
+        obj = mlab.points3d(structured_data[0], structured_data[1], structured_data[2],sigma,mode='cube', scale_mode="none",scale_factor=0.3)#mode="cube"
+        bar = mlab.colorbar(object=obj, title="Stress (in Pa)", orientation="vertical")
+        mlab.view(50,50)
+        mlab.roll(5)
+        #mlab.savefig(os.path.dirname(os.path.realpath(__file__))+'\\sim_outputs\\plot_mayavi.jpg', size=[5000,3000])
+        mlab.show()
+    print plot_mayavi([X,Y,Z,vMs])
     return Mstructure
