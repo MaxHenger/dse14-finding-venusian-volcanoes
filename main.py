cd0a37587e9208874aed04320680c35c0aad3e2c
Stefanharmsen <stefanhar@hetnet.nl>
2016-05-19 22:15:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
 __author__ = 'Stefan'
 import numpy as np
-
-
-rho=1000.   #material density
-g=10. #grav acceleration
-t=0.001
-Volume=10.
-L=4.
-
-Lift1=50000.        #lift depends on everything
-Lift2=10000.
-Loc1=0.5
-Loc2=3.
-#print fuselageloadcase(rho,g,R,L,t,Lift1,Lift2,Loc1,Loc2)
-#assumptions: fuselage=cilinder+spherical ends, 150 kg of other material in fuselage, only Lift in wing, no weight
-
-
+from mayavi import mlab
+from itertools import chain
 ############main##################################333
-rhofuselage=4000.
+rhofuselage=1440.
 rhowing=5000.
-Efuselage=500000.
-Ewing=500000.
+sufuselage=1500000000.
+suwing=500000.
 V=40.
-Rhoair=2.
-Volume=4.
+rhoair=2.
+Volume=20.
 L=5.
-Loc1=3.
+Loc1=1.
 Loc2=4.
-taper=0.6
-cl=1.2#depends on airfoil
-cd=2.4#depends on airfoil
+taper=0.5
+cl=2.#depends on airfoil
+cd=0.4#depends on airfoil
 A=8.
 S=20.
 F=0.004#depends on airfoil,
 g=8.
-SF=2.
-p=3000000   #pressure
+SF=1.5
+p=200000   #pressure
 
 
 
@@ -43,7 +29,7 @@ p=3000000   #pressure
 import fuselage as fs
 
 
-def structuremass(rhofuselage,rhowing,sufuselage,suwing,V,rhoair,Volume, L, Loc1, Loc2,taper,cl,cd,A,S,F,g,SF, p):#A=aspect ratio
+def structuremass(rhofuselage,rhowing,sufuselage,suwing,V,rhoair,Volume, L, Loc1, Loc2,taper,cl,cd,A,S,F,g,SF,p):#A=aspect ratio
     tf=0.0001    #fuselage thickness
     tw=0.0001    #wingbox thickness
     q=1/2*rhoair*V**2
@@ -51,11 +37,13 @@ def structuremass(rhofuselage,rhowing,sufuselage,suwing,V,rhoair,Volume, L, Loc1
     #rhofuselage=    #density fuselage material
     #rhowing=        #density wingbox material
     deviation=1.
+    print R
+    Lift1=100000.#lift1(M,L,)
+    Lift2=1000.#M-Lift1#lift2(M,Lift1)
     while deviation>0.0002:
 
-        Lift1=30000.#lift1(M,L,)
-        Lift2=1000.#M-Lift1#lift2(M,Lift1)
-        fuselageloadcase=fs.fuselageloadcase(rhofuselage,g,R,L,t,Lift1,Lift2,Loc1,Loc2,p)
+
+        fuselageloadcase=fs.fuselageloadcase(rhofuselage,g,R,L,tf,Lift1,Lift2,Loc1,Loc2)
         #wingloadcase=wingloadcase(cl,cd,q,A,taper,S,F,tw,g)#c should depend on amount of lift and aspect ratio, taper ratio, area
         #structure=structure()#moments of inertia, E modules etc.,
         #Vy=wingloadcase[0]
@@ -72,11 +60,12 @@ def structuremass(rhofuselage,rhowing,sufuselage,suwing,V,rhoair,Volume, L, Loc1
         #wingstresses=wingstress(I,Ewin,Vy,Vz,Mz,My,structure,tw,c,SF)
         tf2=fuselagestresses[0]
         #tw2=wingstresses[0]
-        deviationfuselage=np.absolute(tf2-tf)
+        deviationfuselage=abs(tf2-tf)
         #deviationwing=np.absolute(tw2-tw)
         deviation=deviationfuselage#+deviationwing
         tf=tf2
         #tw=tw2
+        print tf
 
     #Mf=fuselagemass(t,L,R,rhofuselage)
     #Mw=wingmass(,,rhowing)#multiply with constant for stuff other than wingbox
@@ -87,15 +76,13 @@ def structuremass(rhofuselage,rhowing,sufuselage,suwing,V,rhoair,Volume, L, Loc1
     z=np.array(fuselage[3])
     vMs=np.array(fuselage[4])
 
-    from itertools import chain
-    X=list(chain.from_iterable(x))
-    Y=list(chain.from_iterable(y))
-    Z=list(chain.from_iterable(z))
-    #print np.shape(Y)
-    #print np.shape(Z)
-    #print np.shape(vMs)
-    return tf
-    print plot_mayavi([X,Y,Z,vMs])
-    #return Mstructure
+    x2=list(chain.from_iterable(x))
+    y2=list(chain.from_iterable(y))
+    z2=list(chain.from_iterable(z))
 
-print structuremass(1440.,0.,1500000000,.0,3.,3.,20., 5., 1., 4.,0.5,2,0.5,5.,30,0.,8.,2,8*10**5)
+    print fs.plot_mayavi([x2,y2,z2,vMs])
+
+    return tf
+    #return Mstructure
+print structuremass(rhofuselage,rhowing,sufuselage,suwing,V,rhoair,Volume, L, Loc1, Loc2,taper,cl,cd,A,S,F,g,SF,p)
+#print structuremass(1440.,0.,1500000000,.0,3.,3.,20., 5., 1., 4.,0.5,2.,0.5,5.,30.,0.,8.,2.,3000000.)
