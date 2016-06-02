fdb4c9a4a4b0f4403c485eb93900356d29b21155
juriasu <weinmiller@live.co.uk>
2016-06-02 18:40:30 +0200

    Added Newtonian Fluid Flow calcualtor
    Fuck Erwin

--- EntryAnalytical.py
+++ EntryAnalytical.py
@@ -3,20 +3,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-g0=8.87
+#g0=8.87
+ge=9.81
 rho0=65.
 H=15.9*1000
 beta=1/H
 GM = 0.32486*10**6*10**9
 
 h       = np.arange(0,300000)#70000. # m 
-m       = 2000+700   # kg
-CL      = 0.2
-CD      = 0.7   # - # 0.7 for ballistic
+m       = (2000+700)  # kg
+CD      = 0.1   # - # 0.7 for ballistic
 S       = np.pi*(4.6/2)**2     # m26051
-gamma   =-30 *np.pi/180 # rad
+gamma   =-10 *np.pi/180 # rad
 Ve      = 13000.
 Re      = 6052.*1000
+
+l_cone = 2
+r_nose = 0.5
+r_base = 4.6
+angle = np.tan((r_base-r_nose)/l_cone) 
+print angle*180/np.pi
+
+CD = (1-np.sin(angle)**4)*(r_nose/r_base)**2+2*np.sin(angle)**2*(1-(r_nose/r_base)**2*np.cos(angle)**2)
+print CD
 
 def g(h):
     g = GM/(Re+h)**2
@@ -25,49 +34,49 @@ def g(h):
 def ballistic():
     V = Ve*np.e**(0.5*g(h)*rho0*np.e**(-beta*h)/(  abs(m*g(h))/(CD*S) * beta * np.sin(gamma) ) )
     a = beta * np.sin(gamma) * Ve**2 * (V/Ve)**2 * np.log(V/Ve)
-    return a/9.81,V
-    
-
-def gliding():
-    V = Ve * np.sqrt( (m*g(h)/S)/CL / ( 0.5*rho0*np.e**(-beta*h)*Ve**2 + (m*g(h)/S)/CL )  ) 
-    Vc = np.sqrt(g(h)*(Re+h))
-    #gamma = (-1/(beta)*2/(CL/CD)*g(h)/V**2)
-    rho = 2* (m*g(h)/S)/CL*(1/V**2 - 1/Vc**2)
-    gamma = np.arcsin( - 4/(beta*rho)*(m*g(h)/S)/CL * 1/V**3 * CD/CL*g(h)*(1-V**2/Vc**2)/V  )    
-    #a = +CD/CL*(1-V**2/Vc**2 - 2/(beta*Re)*1/V**2/Vc**2) 
     a_der = (np.insert(V,len(V),V[-1] )-np.insert(V,0,V[0]) )[:len(V)]*V*gamma
-    #a = -CD/(CL* 1/(1-V**2/Vc**2)) +(gamma)
-    a = -CD/(CL* 1/(1-V**2/Vc**2))
-    return a,a_der/9.81,gamma,Vc,V,rho
+    return a,a_der,V
     
-#
-gout,V = ballistic()
-gfilt = gout[~np.isnan(gout)]
-print max(gfilt)
+def plot_ballistic():
+    aout,a_der,V = ballistic()
+    #afilt = aout[~np.isnan(aout)]
+    #print max(afilt/ge)
 
-plt.plot(gout,h)
-
-glide=False
-if glide:
-    a,a_der,gamma,Vc,V,rho = gliding()
-    figs=plt.figure("Step_response", figsize=(18,10))
-    ax2 = figs.add_subplot(221)
-    ax3 = figs.add_subplot(223)
-    ax4 = figs.add_subplot(222)
-    ax5 = figs.add_subplot(224)
+    figb=plt.figure("Ballistic Entry", figsize=(18,10))
+    ax2 = figb.add_subplot(221)
+    ax3 = figb.add_subplot(223)
+    ax4 = figb.add_subplot(222)
+    ax5 = figb.add_subplot(224)
     
-    ax2.plot(h,V, label = r"$Velocity$")
+#    vely = np.sin(np.deg2rad(gamma))*V
+#    T  = h/vely
+#    T-=T[-1]
+#    print len(vely)
+#    print len(T)
+#    print V
+    
+    ax2.plot(V,h, label = r"$Velocity$")
     ax2.set_title(r"Velocity versus height", fontsize=14)
-    ax2.set_ylabel(r"Velocity in $\frac{m}{s}$", fontsize=14)
-    ax2.set_xlabel(r"height in $s$", fontsize=14)
+    ax2.set_xlabel(r"Velocity in $\frac{m}{s}$", fontsize=14)
+    ax2.set_ylabel(r"height in $m$", fontsize=14)
     ax2.grid(True)
     
-    ax3.plot(h,a, label = r"$acceleration$")
-    ax3.plot(h,gamma, label = r"$gamma$") 
-    ax3.plot(h,a_der, label = r"$acceleration der$") 
+#    ax4.plot(T,h, label = r"$alt$")
+#    ax4.set_title(r"Time vs Height", fontsize=14)
+#    ax4.set_xlabel(r"Time in $s$", fontsize=14)
+#    ax4.set_ylabel(r"height in $m$", fontsize=14)
+#    ax4.grid(True)
+    
+    ax3.plot(aout/ge,h, label = r"$acceleration$")
+    ax3.plot(-a_der/ge,h, label = r"$acceleration der$") 
     ax3.set_title(r"Acceleration in g versus height", fontsize=14)
-    ax3.set_ylabel(r"Acceleration a/g", fontsize=14)
-    ax3.set_xlabel(r"height in $s$", fontsize=14)
+    ax3.set_xlabel(r"Acceleration a/g", fontsize=14)
+    ax3.set_ylabel(r"height in $m$", fontsize=14)
     ax3.legend(loc=2)
     ax3.grid(True)
+    
+    figb.tight_layout()
+    figb.show()
 
+
+plot_ballistic()
---
+++ EntryNewtonian.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 02 16:23:00 2016
+
+@author: Julius
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Newtonian():
+    def __init__(self,points,Mach):
+        self.geometry(points)
+        self.gam = 1.2941
+        self.Mach=Mach
+        self.CP_max = self.CP(self.Mach,self.gam)
+        
+    def CP(self,Mach,gamma):
+        return ( (self.pfrac(Mach,gamma)) -1 )/( (gamma/2.)*Mach**2)
+    
+    def pfrac(self,Mach,gamma):
+        return (2*gamma*Mach**2 - (gamma-1))/(gamma+1)
+        
+    def gamma(self,rho0,rho1,Mach):
+        n=rho1/rho0
+        self.gam = (n+1)/(n-1) - 2*n/( (n-1)**Mach**2 )
+        
+    def CA(self,theta,alpha):
+        return self.CP_max*np.sin(theta)**2 + np.sin(alpha)**2*(1-3*np.sin(theta)**2)
+        
+    def CN(self,theta,alpha):
+        return np.cos(theta)**2*np.sin(2*alpha)
+        
+    def CM(self,theta,alpha):
+        return -2./3*(self.CN(theta,alpha)/(np.tan(theta)*np.cos(theta)**2 ))
+        
+    def analyse(self,alpha,mode="deg"):
+        if mode=="deg":
+            alpha=np.deg2rad(alpha)
+#        for i in range(1,len(self.points[0])-1):
+#            print i
+#            self.theta[i]
+#            CAi=self.CA(self.theta[i],alpha)*(self.points[1][i]**2-self.points[1][i-1]**2)/self.points[1][-1]**2
+#            print CAi
+        
+        self.CA_T = sum([ \
+            self.CA(self.theta[i],alpha)*(self.points[1][i]**2-self.points[1][i-1]**2)/self.points[1][-1]**2 \
+            for i in range(0,len(self.points))])
+        
+        self.CN_T = sum([ \
+            self.CN(self.theta[i],alpha)*(self.points[1][i]**2-self.points[1][i-1]**2)/self.points[1][-1]**2 \
+            for i in range(0,len(self.points))])
+        
+        self.CM_T = self.CM(self.theta[0],alpha) +  sum([ \
+            self.CM(self.theta[i],alpha)*(self.points[1][i]**3-self.points[1][i-1]**3)/self.points[1][-1]**3 \
+            -self.CN(self.theta[i],alpha)*(self.points[1][i]**2-self.points[1][i-1]**2)*(self.s[i]-self.points[0][i])/self.points[1][-1]**3 \
+            for i in range(1,len(self.points))])
+                
+        return self.CA_T, self.CN_T, self.CM_T
+                
+                
+    def geometry(self,points):
+        self.points=np.array(points)
+        self.x=self.points[0]
+        self.y=self.points[1]
+        self.dx=self.x[1:]-self.x[:-1]
+        self.dy=self.y[1:]-self.y[:-1]
+        self.theta=np.tan( self.dy/self.dx )
+        self.s = self.y[1:]/(self.dy/self.dx)
+        
+    def show(self):
+        plt.plot(self.x,self.y)
+        plt.plot(self.x,-self.y)
+        plt.show()
+        
+    def CAa_plot(self):
+        a = np.arange(-5,10,0.1)
+        CA=[]
+        for i in a:
+            CA.append(self.analyse(i)[0])
+        plt.plot(a,CA)
+        plt.show()
+        
+    def CNa_plot(self):
+        a = np.arange(-5,10,0.1)
+        CN=[]
+        for i in a:
+            CN.append(self.analyse(i)[1])
+        plt.plot(a,CN)
+        plt.show()
+        
+    def CMa_plot(self):
+        a = np.arange(-5,10,0.1)
+        CM=[]
+        for i in a:
+            CM.append(self.analyse(i)[2])
+        plt.plot(a,CM)
+        plt.show()
+        
+        
+def test_shield():
+    
+    y = lambda x: 2*x**0.4
+    x = np.arange(0,2+0.01,0.01)
+    points=[x,y(x)]
+    Mach = 10.
+    shield = Newtonian(points,Mach)
+
+    return shield
+    
+    
+if __name__=="__main__":
+    test=test_shield()
+    test.analyse(10)
+    #test.show()
+    print test.CA_T
+    print test.CN_T
+    print test.CM_T
+    test.CAa_plot()
+    
